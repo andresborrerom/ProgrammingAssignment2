bad05d5daaf38c84cd6e17369637740a42f7b2e6
Malostragos <andres.borrerom@gmail.com>
2014-04-22 18:52:09 -0500

    Putting first own version of cachematrix.R for PeerAssessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Given the delay on running the solve function to find the inverse of a matrix,
+## these functions will cache a given matrix and its inverse by creating
+## a list with cached values
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates and caches a matrix and spaces in the environment 
+## to cache the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)  
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve, when called for first time with a given matrix, finds the inverse
+## of the matrix and stores (caches) it in the list created by running first the 
+## makeCacheMatrix function. From there on, it finds the cached value and returns
+## it without calculating it again.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
